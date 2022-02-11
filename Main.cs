2ecdd0afb4f271ef8a4c9f547c3a9190ceaaaaad
Decider <90727418+xDecider@users.noreply.github.com>
2022-02-11 20:09:08 +0300

    Questies got no testies :(

---
+++ Main.cs
@@ -0,0 +1,72 @@
+﻿using HarmonyLib;
+using MelonLoader;
+using System.Reflection;
+
+namespace anti_quest
+{
+    public class Main : MelonMod
+    {
+
+	    public override void OnApplicationStart()
+	    {
+		    try
+		    {
+			    InitPatch();
+			    MelonLogger.Msg("[Anti Quest] Loaded Anti-Quest Mod, Enjoy! (https://github.com/xDecider)");
+		    }
+		    catch
+		    {
+		    }
+	    }
+	    
+	    
+	    private static HarmonyMethod GetPatch(string name)
+	    {
+		    return new HarmonyMethod(typeof(Main).GetMethod(name, BindingFlags.Static | BindingFlags.NonPublic));
+	    }
+	    
+	    private static readonly HarmonyLib.Harmony Instance = new HarmonyLib.Harmony("Nemesis");
+
+	    private static void CreatePatch(MethodInfo TargetMethod, HarmonyMethod Before = null, HarmonyMethod After = null)
+	    {
+		    try
+		    {
+			    Instance.Patch(TargetMethod, Before, After);
+		    }
+		    catch (System.Exception e)
+		    {
+			    MelonLogger.Error($"Failed to Patch {TargetMethod.Name} \n{e}");
+		    }
+	    }
+
+	    private static void InitPatch()
+	    {
+		    try
+		    {
+			    CreatePatch(typeof(NetworkManager).GetMethod("Method_Public_Void_Player_0"), GetPatch("PrettyGay"));
+		    }
+		    catch (System.Exception ex)
+		    {
+			    MelonLogger.Error("Error while Patching OnPlayerJoin!", ex);
+		    }
+	    }
+	    
+
+	    private static void PrettyGay(ref VRC.Player __0)
+	    { 
+		    try
+		    {
+			    if (__0.field_Private_APIUser_0.IsOnMobile)
+			    {
+				    __0.gameObject.SetActive(value: false);
+				    MelonLogger.Msg("[AntiQuest] Locally Blocked Quest Player ---> " + __0.prop_APIUser_0.displayName, System.ConsoleColor.Red);
+			    }
+		    }
+		    catch
+		    {
+		    }
+	    }
+	    
+	    
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using MelonLoader;
+
+[assembly: AssemblyTitle("AntiQuest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AntiQuest")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("13b85d59-ca85-44c8-8e3c-4d180de87538")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: MelonInfo(typeof(anti_quest.Main), "Anti Quest", "1.0.0", "Decider", "https://www.decider.wtf")]
+[assembly: MelonColor(ConsoleColor.DarkYellow)]
+[assembly: MelonGame("VRChat", "VRChat")]
